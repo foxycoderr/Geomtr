dae660072eb9c9538486cab649f7341674df886f
foxycoderr <djags08@gmail.com>
2023-08-10 21:21:00 +0200

    Added explanation to lint file.

--- lint.py
+++ lint.py
@@ -1,3 +1,19 @@
+"""
+Pylint Action Configuration File
+
+Controls how the PyLint action that automatically runs upon a commit functions. The action itself
+can be found in Geomtr/.github/workflows/pylint.yml. This configuration file allows different
+fail thresholds, the default is 7, but the threshold is controlled in the YML file. It is currently
+set to 8.
+
+"""
+
+# Thanks to doedotdev for this code; you can read his article containing this code at:
+# https://medium.com/analytics-vidhya/
+# pylint-static-code-analysis-github-
+# action-to-fail-below-a-score-threshold-58a124aafaa0
+
+
 import argparse
 from pylint.lint import Run
 
