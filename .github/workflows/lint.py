996dd54af73137b112a26e492c1bc16d5a60e15c
FoxyCoder <103729245+foxycoderr@users.noreply.github.com>
2023-08-10 19:03:07 +0200

    Create lint.py

---
+++ .github/workflows/lint.py
@@ -0,0 +1,46 @@
+import argparse
+from pylint.lint import Run
+
+parser = argparse.ArgumentParser(prog="LINT")
+
+parser.add_argument('-p',
+                    '--path',
+                    help='path to directory you want to run pylint | '
+                         'Default: %(default)s | '
+                         'Type: %(type)s ',
+                    default='./src',
+                    type=str)
+
+parser.add_argument('-t',
+                    '--threshold',
+                    help='score threshold to fail pylint runner | '
+                         'Default: %(default)s | '
+                         'Type: %(type)s ',
+                    default=7,
+                    type=float)
+
+args = parser.parse_args()
+path = str(args.path)
+threshold = float(args.threshold)
+
+results = Run([path], do_exit=False)
+
+final_score = results.linter.stats['global_note']
+
+if final_score < threshold:
+
+    message = ('PyLint Failed | '
+               'Score: {} | '
+               'Threshold: {} '.format(final_score, threshold))
+
+    logging.error(message)
+    raise Exception(message)
+
+else:
+    message = ('PyLint Passed | '
+               'Score: {} | '
+               'Threshold: {} '.format(final_score, threshold))
+
+
+
+    exit(0)
